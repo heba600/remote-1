97c0b9f010459d99b052d019580565909b37840e
Heba Othman <60092554@udst.edu.qa>
2024-01-31 10:43:00 +0300

    Add new function

--- cars.py
+++ cars.py
@@ -6,6 +6,12 @@ de main ():
 	randomBrands=random.choice(carBrands)
 	print("Car Brand: " , randomBrand)
 
+
+def get_model_year():
+	return random.randint(1990,2024)
+
+
+
 if _ _name_ _=="_ _main_ _":
 	main()
 
