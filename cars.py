ab273b2ccc5d82b8f54d29d39bb41c5107ded644
Heba Othman <60092554@udst.edu.qa>
2024-01-31 10:34:06 +0300

    Added random brands for cars

--- cars.py
+++ cars.py
@@ -1,5 +1,10 @@
+
+import random
+
 de main ():
-	print("Cars Program")
+	carBrands=["Toyota", "BMW" , "Mercedes" , "Lexus" , "Nissan" , "Kia"]
+	randomBrands=random.choice(carBrands)
+	print("Car Brand: " , randomBrand)
 
 if _ _name_ _=="_ _main_ _":
 	main()
